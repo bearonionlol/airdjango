573459af837c0da06c167ad0e3ad2a5a94a8b851
Brian Perrett <perrettbrian@gmail.com>
2020-02-10 17:57:29 -0700

    generalize imports

--- airdjango/airscrape/models.py
+++ airdjango/airscrape/models.py
@@ -2,9 +2,32 @@ from django.db import models
 
 
 class Price(models.Model):
-    stores_and_prices = models.CharField(max_length=200)
-    cheapest_price = models.CharField(max_length=200)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, null=True)
+    store = models.ForeignKey('Store', on_delete=models.CASCADE, null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+
+    class Meta:
+        unique_together = ('product', 'store')
 
     def __str__(self):
-        return f'{self.stores_and_prices}, {self.cheapest_price}'
+        return f'{self.store}, {self.price}'
+
+
+class Product(models.Model):
+    upc = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=512, default='')
+    target_url = models.URLField(max_length=512, unique=True, null=True)
+    frys_url = models.URLField(max_length=512, unique=True, null=True)
+    bestbuy_url = models.URLField(max_length=512, unique=True, null=True)
+
+    def __str__(self):
+        return f'Product(upc={self.upc}, name={self.name})'
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    url = models.URLField(max_length=512, unique=True)
+
+    def __str__(self):
+        return f'Store(name={self.name})'
 
